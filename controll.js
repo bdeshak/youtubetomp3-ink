1088ce39badc10dea7925e367c897a4388ed9327
Eshak <43699326+bdeshak@users.noreply.github.com>
2024-03-03 08:48:49 +0600

    Update controll.js

--- controll.js
+++ controll.js
@@ -1,5 +1,40 @@
 
         $(document).ready(function() {
+
+               // user cookie
+                function userCookie(key, value, expiry){
+                        
+function setCookie(key, value, expiry) {
+        var expires = new Date();
+        expires.setTime(expires.getTime() + (expiry * 1000));
+        document.cookie = key + '=' + value + ';expires=' + expires.toUTCString();
+    }
+    
+    //call setCookie
+    setCookie(key, value, expiry)
+
+    function getCookie(key) {
+        var keyValue = document.cookie.match('(^|;) ?' + key + '=([^;]*)(;|$)');
+        return keyValue ? keyValue[2] : null;
+alert(keyValue);
+
+    }
+    
+   // call getCookie
+getCookie("test");
+
+                        
+    function eraseCookie(key) {
+        var keyValue = getCookie(key);
+        setCookie(key, keyValue, '-1');
+    }
+
+                        
+                }
+                
+                
+//user cookie call
+userCookie("test", "es", 30);
                 
                
                 //cpa redirect
