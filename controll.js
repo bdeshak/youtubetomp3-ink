baecef31e89f1d7d93ec1b09b6ddc8bfd9b9350f
Eshak <43699326+bdeshak@users.noreply.github.com>
2024-03-12 16:20:32 +0600

    Update controll.js

--- controll.js
+++ controll.js
@@ -190,7 +190,7 @@ setCookie("test", "es", 30);
        data.items.items.forEach((obj, i) => {      
            
           alert(obj.id);
-           document.getElementById("fetchData").innerHTML += `<img src="">${obj.id}</img>`;
+           document.getElementById("fetchData").innerHTML += `<tr><td><img src="">${obj.id}</img></td></tr>`;
          
            });
        
